4f117864ec24f1ddeb3c954ada1556cc2104704d
Taher Alkhateeb <taher@pythys.com>
2021-03-30 20:51:27 +0300

    minor syntax refactoring

--- src/main/java/org/spicetrade/Game.java
+++ src/main/java/org/spicetrade/Game.java
@@ -1,18 +1,18 @@
 /* Spice Trade
  * Copyright (C) 2005 spicetrade.org
- *  
+ *
  * Author: Juha Holopainen, juhah@spicetrade.org
- *   
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License
  * as published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
-   
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Library General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Library General Public
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -28,19 +28,17 @@ import javax.swing.UIManager;
 /**
  * Spice Trade
  * This is the main class that starts the whole application.
- * 
+ *
  * @author Juha Holopainen
  * @version 1.0
  */
 
-public class Game
-{
+public class Game {
 
-    public Game()
-    {
+    public Game() {
         Frame frame = new Mainframe();
         // I heard that macs have problems with this undecorated setting, will look into that
-        frame.setUndecorated(true);        
+        frame.setUndecorated(true);
         frame.setLayout(null);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation((d.width - frame.getWidth()) / 2, (d.height - frame.getHeight()) / 2);
@@ -50,8 +48,7 @@ public class Game
         frame.setBackground(Color.lightGray);
         frame.addWindowListener(new WindowAdapter() {
 
-            public void windowClosing(WindowEvent e)
-            {
+            public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
 
@@ -61,14 +58,10 @@ public class Game
         Mainframe.me.gotoPlace("intro");
     }
 
-    public static void main(String args[])
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         new Game();
