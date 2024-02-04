94947f2c4b70cd4ece895673fd25bb2b82eb9772
Taher Alkhateeb <taher@pythys.com>
2024-02-04 13:41:48 +0300

    add groovy scripts to aid with audio debugging

---
+++ debug/MixersFormats.groovy
@@ -0,0 +1,33 @@
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.DataLine
+import groovy.json.JsonOutput
+
+def mixerInfos = AudioSystem.getMixerInfo() as List
+mixerInfos.findAll{info -> info.getName().contains('Generic_1')}.each { mixerInfo ->
+    def mixerMap = [
+        name: mixerInfo.getName(),
+        vendor: mixerInfo.getVendor(),
+        version: mixerInfo.getVersion(),
+        description: mixerInfo.getDescription()
+    ]
+    def mixer = AudioSystem.getMixer(mixerInfo)
+    def sourceLineInfo = mixer.getSourceLineInfo()
+    sourceLineInfo.each { info ->
+        if (info instanceof DataLine.Info) {
+            def dataLineInfo = (DataLine.Info) info
+            def supportedFormats = dataLineInfo.formats
+            supportedFormats.each { format ->
+                def formatMap = [
+                    *:mixerMap,
+                    sampleRate: format.sampleRate,
+                    sampleSizeInBits: format.sampleSizeInBits,
+                    channels: format.channels,
+                    frameSize: format.frameSize,
+                    frameRate: format.frameRate,
+                    bigEndian: format.bigEndian
+                ]
+            println(JsonOutput.prettyPrint(JsonOutput.toJson(formatMap)))
+            }
+        }
+    }
+}
